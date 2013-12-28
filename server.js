c33e9aeec0272fd25554cd854088aa6d535a3464
Sergey Khokhlov <worldfly@yandex.ru>
2013-12-28 16:19:00 +0400

    Add 414 error

--- server.js
+++ server.js
@@ -11,23 +11,39 @@ app.get('/:name/:version/:type/:file', function (req, res) {
         type: req.params.type,
         file: req.params.file
     };
-    res.sendfile(__dirname + '/static/' + adress.name + '/' + adress.type + '/' + adress.file);
+    if(req.originalUrl.length <= 40 ){
+            res.sendfile(__dirname + '/static/' + adress.name + '/' + adress.type + '/' + adress.file);
+    } else {
+        error414(req, res);
+    }
 });
 
 app.get('/', function (req, res) {
-    res.set('Content-Type', 'text/html');
-    res.sendfile(__dirname + '/server/index.html');
+    if(req.originalUrl.length <= 40){
+        res.set('Content-Type', 'text/html');
+        res.sendfile(__dirname + '/server/index.html');
+    } else {
+        error414(req, res);
+    }
 });
 
 app.get('/robots.txt', function (req, res) {
-    res.set('Content-Type', 'text/plain');
-    res.sendfile(__dirname + '/server/robots.txt');
+    if(req.originalUrl.length <= 40){
+        res.set('Content-Type', 'text/plain');
+        res.sendfile(__dirname + '/server/robots.txt');
+    } else {
+        error414(req, res);
+    }
 });
 
 app.use(function (req, res) {
-    res.status(404);
-    res.set('Content-Type', 'text/html');
-    res.sendfile(__dirname + '/server/404.html');
+    if(req.originalUrl.length <= 40){
+        res.status(404);
+        res.set('Content-Type', 'text/html');
+        res.sendfile(__dirname + '/server/404.html');
+    } else {
+        error414(req, res);
+    }
 });
 
 http.createServer(app).listen(app.get('port'), function () {
@@ -42,3 +58,9 @@ http.createServer(app).listen(app.get('port'), function () {
 //    }
 //    return response;
 //}
+
+function error414(req, res){
+    res.set('Content-Type', 'text/html');
+    res.status(414);
+    res.sendfile(__dirname + '/server/414.html');
+}
